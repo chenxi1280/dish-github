a50f0eab746969b86bae0f9b7746fc5650d53376
SJ <1366314370@qq.com>
2020-08-25 18:47:49 +0800

    fixed and add

--- pages/login/login/login.vue
+++ pages/login/login/login.vue
@@ -1,6 +1,31 @@
 <template>
 	<view class="login">
-		<view class="redirect_btn" @click="toSigninPage">点击前往注册页面</view>
+		<view class="top">
+			<view class="login_redirect_btn">登录
+				<view>Welcome</view>
+			</view>
+			<view class="signin_redirect_btn" @click="toSigninPage">注册</view>
+		</view>
+		<view class="bottom">
+			<form @submit="formSubmit">
+				<view class="phoneBox">
+					<icon class="phoneIcon"></icon>
+					<input class="acount" type="text" placeholder="输入手机号"/>
+				</view>
+				<view class="pwdBox">
+					<icon  class="pwdIcon"></icon>
+					<input class="pwd" password placeholder="密码"/>
+				</view>
+				<view class="codeBox">
+					<input class="code" />
+					<view class="codeImage">
+						<image src=""></image>
+					</view>
+				</view>
+				<!-- form-type="submit" 点击按钮提交表单 -->
+				<button class="login" type="default" plain="true" form-type="submit">登录</button>
+			</form>
+		</view>
 	</view>
 </template>
 
@@ -22,9 +47,103 @@
 </script>
 
 <style lang="scss">
+	page{
+		height: 100%;
+	}
 	.login {
-		.redirect_btn {
-			color: red;
+		height: 100%;
+		background-color: #F08080;
+		.top{
+			display: flex;
+			justify-content: space-between;
+			height: 500rpx;
+			width: 750rpx;
+			.login_redirect_btn {
+				color: #FCFCFC;
+				font-size: 50rpx;
+				font-weight: bold;
+				margin-left: 100rpx;
+				margin-top: 100rpx;
+			}
+			.signin_redirect_btn{
+				color: #FCFCFC;
+				font-weight: bold;
+				margin: 20rpx;
+			}
+		}
+		.bottom{
+			.phoneBox{
+				border: 2rpx solid black;
+				height: 92rpx;
+				border-radius: 10rpx;
+				width: 670rpx;
+				margin: 0 auto;
+				margin-top: 100rpx;
+				display: flex;
+				justify-content: flex-start;
+				.acount{
+					width: 100%;
+					height: 100%;
+				}
+				.phoneIcon{
+					background: url(../../../static/icon/手机.png) no-repeat center;
+					width: 80rpx;
+					height: 92rpx;
+					background-size: 60rpx;
+				}
+			}
+			.pwdBox{
+				width: 670rpx;
+				height: 92rpx;
+				border: 2rpx solid black;
+				border-radius: 10rpx;
+				margin: 0 auto;
+				margin-top: 50rpx;
+				display: flex;
+				justify-content: flex-start;
+				.pwdIcon{
+					background: url(../../../static/icon/密码.png) no-repeat center;
+					width: 80rpx;
+					height: 80rpx;
+					background-size: 60rpx;
+				}
+				.pwd{
+					width: 100%;
+					height: 100%;
+				}
+			}
+			.codeBox{
+				width: 670rpx;
+				height: 92rpx;
+				/* border: 2rpx solid black; */
+				border-radius: 10rpx;
+				margin: 0 auto;
+				margin-top: 50rpx;
+				display: flex;
+				justify-content: space-between;
+				.code{
+					border: 2rpx solid black;
+					border-radius: 10rpx;
+					height: 100%;
+					width: 60%;
+					padding-left: 40rpx;
+				}
+				.codeImage{
+					border: 2rpx solid black;
+					border-radius: 10rpx;
+					height: 100%;
+					width: 30%;
+				}
+			}
+			.login{
+				&:hover{
+					background-color: #EBEBEB;
+				}
+				border: 2rpx solid black;
+				width: 670rpx;
+				margin: 0 auto;
+				margin-top: 50rpx;
+			}
 		}
 	}
 </style>
--- pages/login/signin/signin.vue
+++ pages/login/signin/signin.vue
@@ -1,6 +1,38 @@
 <template>
-	<view>
-		注册页面
+	<view class="signin">
+		<view class="top">嗨！你好
+				<view>欢迎注册四刻互动视频</view>
+		</view>
+		<view class="content">
+			<form @submit="formSubmit">
+				<view class="userBox">
+					<input class="userName" type="text" placeholder="昵称"/>
+				</view>
+				<view class="userBox">
+					<input class="phone" type="text" placeholder="手机号"/>
+				</view>
+				<view class="userBox">
+					<input class="pwd" password placeholder="密码"/>
+				</view>
+				<view class="userBox">
+					<input class="confirmPwd" password placeholder="确认密码"/>
+				</view>
+				<view class="codeBox">
+					<input class="code" />
+					<view class="codeImage">
+						<image src=""></image>
+					</view>
+				</view>
+				<view class="phoneCodeBox">
+					<input class="phoneCode" />
+					<view class="button">
+						<button class="getPhoneCode" plain="true">获取短信验证码</button>
+					</view>
+				</view>
+				<!-- form-type="submit" -->
+				<button class="signin-btn" type="default" plain="true" >注册</button>
+			</form>
+		</view>
 	</view>
 </template>
 
@@ -17,6 +49,120 @@
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+	.signin{
+		height: 100%;
+		background-color: #F08080;
+		overflow: hidden;
+		.top{
+			height: 200rpx;
+			width: 750rpx;
+			color: #FCFCFC;
+			font-size: 40rpx;
+			font-weight: bold;
+			margin-left: 100rpx;
+			margin-top: 100rpx;
+		}
+		
+		.codeBox{
+			width: 670rpx;
+			height: 92rpx;
+			/* border: 2rpx solid black; */
+			border-radius: 10rpx;
+			margin: 0 auto;
+			margin-top: 50rpx;
+			display: flex;
+			justify-content: space-between;
+			.code{
+				border: 2rpx solid black;
+				border-radius: 10rpx;
+				height: 100%;
+				width: 60%;
+				padding-left: 40rpx;
+			}
+			.codeImage{
+				border: 2rpx solid black;
+				border-radius: 10rpx;
+				height: 100%;
+				width: 30%;
+			}
+		}
+		
+		.phoneCodeBox{
+			width: 670rpx;
+			height: 92rpx;
+			/* border: 2rpx solid black; */
+			border-radius: 10rpx;
+			margin: 0 auto;
+			margin-top: 50rpx;
+			display: flex;
+			justify-content: space-between;
+			.phoneCode{
+				border: 2rpx solid black;
+				border-radius: 10rpx;
+				height: 100%;
+				width: 30%;
+				padding-left: 40rpx;
+			}
+			.button{
+				border-radius: 10rpx;
+				height: 100%;
+				width: 60%;
+				.getPhoneCode{
+					font-size: 30rpx;
+					width: 100%;
+					height: 100%;
+				}
+			}
+		}
+		
+		.userBox{
+			width: 670rpx;
+			height: 92rpx;
+			border: 2rpx solid black;
+			border-radius: 10rpx;
+			margin: 0 auto;
+			display: flex;
+			justify-content: flex-start;
+			.userName{
+				width: 100%;
+				height: 100%;
+			}
+		}
+		
+		.userBox{
+			width: 670rpx;
+			height: 92rpx;
+			border: 2rpx solid black;
+			border-radius: 10rpx;
+			margin: 0 auto;
+			margin-top: 50rpx;
+			display: flex;
+			justify-content: flex-start;
+			.phone{
+				width: 100%;
+				height: 100%;
+			}
+			.pwd{
+				width: 100%;
+				height: 100%;
+			}
+			.confirmPwd{
+				width: 100%;
+				height: 100%;
+			}
+		}
+		
+		.content{
+			.signin-btn{
+				&:hover{
+					background-color: #EBEBEB;
+				}
+				border: 2rpx solid black;
+				width: 670rpx;
+				margin: 0 auto;
+				margin-top: 50rpx;
+			}
+		}
+	}
 </style>
