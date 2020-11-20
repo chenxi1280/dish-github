74f5adf141daf9eca0f0d74e607d3cb308ca6f62
SJ <1366314370@qq.com>
2020-11-20 13:42:50 +0800

    fix config.js

--- pages/login/config/config.js
+++ pages/login/config/config.js
@@ -1,6 +1,6 @@
 /* 测试环境环境 */
-export const baseURL = 'https://wanxiangchengzhen.com/wxpi'
+// export const baseURL = 'https://wanxiangchengzhen.com/wxpi'
 // export const baseURL = 'http://192.168.1.10:8080'
 // export const baseURL = 'http://192.168.1.15:8009'
 /* 正式环境 */
-// export const baseURL = 'https://wanxiangchengzhen.com/pwxpi'
+export const baseURL = 'https://wanxiangchengzhen.com/pwxpi'
