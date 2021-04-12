964b33b9378ce837511cca0e8a1fa9f9ccd4a567
zhengrui <2290715547@qq.com>
2021-04-12 09:02:19 +0800

    ...

--- pages/login/config/config.js
+++ pages/login/config/config.js
@@ -1,7 +1,7 @@
 /* 测试环境环境 */
-export const baseURL = 'https://wanxiangchengzhen.com/wxpi'
+// export const baseURL = 'https://wanxiangchengzhen.com/wxpi'
 // export const baseURL = 'http://192.168.1.10:8080'
 // export const baseURL = 'http://192.168.1.3:8080'
 /* 正式环境 */
-// export const baseURL = 'https://wanxiangchengzhen.com/pwxpi'
+export const baseURL = 'https://wanxiangchengzhen.com/pwxpi'
 export const versionId = 2.5
