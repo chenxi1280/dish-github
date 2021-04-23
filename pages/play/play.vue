6d8bae7a8129723bfaa476d005c6a9af84637b20
zhengrui <2290715547@qq.com>
2021-04-23 14:33:44 +0800

    ....

--- pages/play/play.vue
+++ pages/play/play.vue
@@ -4719,7 +4719,7 @@
       top: 0;
       width: 100%;
       height: 100%;
-      z-index: 9999;
+      z-index: 999;
 
       .chooseTipsMask16 {
         background-color: rgba(255, 255, 255, 0);
