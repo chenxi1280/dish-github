afac440db50f1cac5012148519cfe567bea254ca
SJ <1366314370@qq.com>
2021-05-27 13:51:14 +0800

    fix vr

--- pages/login/config/config.js
+++ pages/login/config/config.js
@@ -1,7 +1,9 @@
-/* 测试环境环境 */
+/* 测试环境 */
 export const baseURL = 'https://wanxiangchengzhen.com/wxpi'
 // export const baseURL = 'http://192.168.1.10:8080'
 // export const baseURL = 'http://192.168.1.3:8080'
 /* 正式环境 */
 // export const baseURL = 'https://wanxiangchengzhen.com/pwxpi'
-export const versionId = "3.2121"
+/* VR测试环境 */
+export const vrBaseURL = 'https://wanxiangchengzhen.com/cooookie/#/pages/mine/vr/vr'
+export const versionId = "3.2122"
